b6e33c684116335f6317ce1eebe2ea1b86c6e581
WahidHoquee <wahidhoquee@gmail.com>
2021-10-01 15:36:14 +0600

    Disabled Apperance Pages

--- layout/_sidebar.tsx
+++ layout/_sidebar.tsx
@@ -21,7 +21,7 @@ const MENU: { [key: string]: string[] } = {
   Blogs: ['Add New Post', 'Blog Post', 'Categories'],
   Review: ['Add New Review', 'Reviews'],
   'Query Report': ['Add New Query', 'Queries'],
-  Appearance: ['Pages', 'Menus', 'Media'],
+  // Appearance: ['Pages', 'Menus', 'Media'],
   Settings: [
     'Accounts',
     'Contact Person',
